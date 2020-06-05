1051515c91bd17980a89bb40ff8a89d3380682c0
kelshmo <kelsey.montgomery@sagebase.org>
2020-06-05 13:27:48 -0700

    fix mocking

--- tests/testthat/test-reject_submission.R
+++ tests/testthat/test-reject_submission.R
@@ -1,20 +1,20 @@
 context("accept-reject-submission.R")
 
 test_that("strings are generated from reject_submission", {
-  m <- mockery::mock("character strings") # i think this should be a string to mock the reason?
+  m <- mockery::mock(TRUE)
   mockery::stub(reject_submission, "rest_put", m)
   test_reject <- reject_submission(syn = "",
-                                   form_data_id = 42,
+                                   form_data_id = 1,
                                    reason = "your submission looks good")
-  testthat::expect_vector(test_reject)
+  testthat::expect_true(test_reject)
 })
 
 test_that("warning for strings more than 500 chars", {
   string <- glue::glue_collapse({seq(1:300)}, sep = ",")
-  m <- mockery::mock(string)
+  m <- mockery::mock(TRUE)
   mockery::stub(reject_submission, "rest_put", m)
   testthat::expect_error(reject_submission(syn = "",
-                                           form_data_id = 42,
+                                           form_data_id = 1,
                                            reason = string)
   )
 
